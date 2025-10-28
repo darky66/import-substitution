1bb5e5309ceac652742a2ad40f647dc6ac150707
vova <213@gmail.com>
2025-10-28 15:59:52 +0300

    add result if cant search add

--- webproject/project/main/data.py
+++ webproject/project/main/data.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 db = pd.read_csv('main/123.csv',  lineterminator='\n')
-print(db.head())
+dict_apps = {}
+k = 0
+for i in db['Иностранное ПО']:
+    dict_apps[i] = dict_apps.get(i, db['Российский аналог'][k].split(', '))
+    k += 1
--- webproject/project/main/views.py
+++ webproject/project/main/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
-from .models import Import
 from .forms import ContactForm
 from django.views.decorators.csrf import requires_csrf_token
-from .data import db
+from .data import dict_apps
 
 @requires_csrf_token
 def main_page(request):
-    qwe = Import.objects.all()
     if request.method == 'POST':
             form = ContactForm(request.POST)
+            request_result = request.POST.get('name')
+            if request_result in dict_apps.keys():
+                result = dict_apps[result]
+            else:
+                result = ['К сожалению, мы не можем найти похожие приложения']
             if form.is_valid():
                 name = form.cleaned_data['name']
-                print(123)
-                return render(request, 'main/main_page.html', { 'qwe': qwe })
+                return render(request, 'main/main_page.html', { 'result': result })
             else:
-                print('qwe')
+                return render(request, 'main/main_page.html', { 'result': result })
     else:
         form = ContactForm()
     return render(request, 'main/main_page.html')
